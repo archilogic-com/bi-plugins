fd2335f28b4f04bb53fae11920f17616b14c87df
malwina-archilogic <117272338+malwina-archilogic@users.noreply.github.com>
2024-07-25 10:42:53 +0200

    feat: useEffect hooks to properly update component's state

--- power-bi/floor-plan/src/components/FloorPanel.tsx
+++ power-bi/floor-plan/src/components/FloorPanel.tsx
@@ -21,11 +21,9 @@ interface FloorPanelProps {
 }
 
 export const FloorPanel = (props: FloorPanelProps) => {
-  const [state, setState] = useState<{ selectedSpace: any; isFloorPlanLoaded: boolean; floorPlan: FloorPlanEngine }>({
-    selectedSpace: null,
-    isFloorPlanLoaded: false,
-    floorPlan: null
-  })
+  const [floorPlan, setFloorplan] = useState(null)
+  const [isFloorPlanLoaded, setIsFloorPlanLoaded] = useState(false)
+  const [selectedSpace, setSelectedSpace] = useState(null)
 
   const getGradientColorBySpaceValue = (space) => {
     const gradient = generateGradients(props.gradient.min.color, props.gradient.max.color)
@@ -53,7 +51,7 @@ export const FloorPanel = (props: FloorPanelProps) => {
   }
 
   const highlightNodes = (fillOpacity = 1.0) => {
-    const nodes = getAssetsAndSpaces(state.floorPlan)
+    const nodes = getAssetsAndSpaces(floorPlan)
     nodes.forEach((space: any) => {
       highlightNode(space, fillOpacity)
     })
@@ -62,24 +60,13 @@ export const FloorPanel = (props: FloorPanelProps) => {
   const highlightNodesFromProps = () => {
     highlightNodes(0.5)
     props.nodeIds.forEach(nodeId => {
-      const space = getNodeById(state.floorPlan, nodeId)
+      const space = getNodeById(floorPlan, nodeId)
       highlightNode(space)
     })
   }
 
-  const handleClick = () => {
-    state.floorPlan?.on('click', event => {
-      const position: number[] = event.pos
-      const selectedSpace = getSpaceByPosition(state.floorPlan, position)
-      if (state.selectedSpace === selectedSpace) return
-      setState(prevState => ({ ...prevState, selectedSpace }))
-      if (!selectedSpace) return
-      props.onClick(selectedSpace.id)
-    })
-  }
-
   const handleHighlightedNodes = () => {
-    if (!state.isFloorPlanLoaded) return
+    if (!isFloorPlanLoaded) return
     if (props.nodeIds.length) {
       highlightNodesFromProps()
     } else {
@@ -88,25 +75,44 @@ export const FloorPanel = (props: FloorPanelProps) => {
   }
 
   const handleLoad = (fpe: FloorPlanEngine) => {
-    setState(prevState => ({
-      ...prevState,
-      isFloorPlanLoaded: true,
-      floorPlan: fpe
-    }))
-    handleHighlightedNodes()
-    handleClick()
+     handleHighlightedNodes()
+     setFloorplan(fpe)
+     setIsFloorPlanLoaded(true)
   }
 
   useEffect(() => {
     handleHighlightedNodes()
   })
 
+  useEffect(() => {
+    if (floorPlan) {
+      const handleClickEvent = event => {
+        const position: number[] = event.pos
+        const spaceByPosition = getSpaceByPosition(floorPlan, position)
+        if (selectedSpace === spaceByPosition) { 
+          return
+        }
+        setSelectedSpace(spaceByPosition)
+        if (!spaceByPosition) {
+          return
+        }
+        props.onClick(spaceByPosition.id)
+      }
+  
+      floorPlan.on('click', handleClickEvent)
+  
+      return () => {
+        floorPlan.off('click', handleClickEvent)
+      }
+    }
+  }, [floorPlan, props.onClick])
+
   return (
     <div id="app">
       <FloorPlan
         id={props.floorID}
         token={props.publishableToken}
-        loaded={state.isFloorPlanLoaded}
+        loaded={isFloorPlanLoaded}
         onLoad={handleLoad}
       />
     </div>
